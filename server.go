5e39590a19d75fa391df8dfd171e433107db5cbe
Honphie.Won <776878251@qq.com>
2025-04-25 02:40:52 +0800

    Show server IP ,port on  URL root directory

--- server.go
+++ server.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"mymodule/xraycoreHelper"
+	"net"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -12,8 +15,73 @@ import (
 
 var xray = &xraycoreHelper.XrayService{}
 
+func getPublicIP() (string, error) {
+	resp, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+// 获取服务器首个非环回 IPv4
+func getServerIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, addr := range addrs {
+		// 只处理 IP 网络地址
+		if ipNet, ok := addr.(*net.IPNet); ok {
+			ip := ipNet.IP
+			// 跳过环回、IPv6
+			if !ip.IsLoopback() && ip.To4() != nil {
+				return ip.String(), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("no non-loopback IPv4 address found")
+}
+
 func main() {
 	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		// 获取服务器 IP
+		serverIP, err := getServerIP()
+		if err != nil {
+			serverIP = "unknown"
+		}
+		// 获取公网 IP
+		publicIP, err := getPublicIP()
+		if err != nil {
+			publicIP = "unknown"
+		}
+
+		// 取监听端口
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+		}
+
+		// 构造 HTML
+		html := fmt.Sprintf(`
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head><meta charset="UTF-8"><title>服务器信息</title></head>
+<body>
+  <p style="font-size:24px; color:#e91e63; font-weight:bold;">
+    您好，服务器内网IP，公网IP 和 端口：<span style="color:#3f51b5">%s,%s:%s</span>
+  </p>
+</body>
+</html>`, serverIP, publicIP, port)
+
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(html))
+	})
 
 	// 启动接口
 	r.GET("/start", func(c *gin.Context) {
